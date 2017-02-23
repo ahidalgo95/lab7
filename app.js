0c38bd89dce6719716da244ce24c1a133936cc67
ahidalgo95 <ahidalgo@ucsd.edu>
2017-02-23 10:15:59 -0800

    new stuff

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
+
 var app = express();
 
 // all environments
@@ -38,6 +39,9 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/project/:id', project.view);
+app.get('/min', index.viewMin);
+
+
 // Example route
 // app.get('/users', user.list);
 
